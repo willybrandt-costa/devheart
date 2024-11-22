0aaa38175b6ade1ecc003999986da72143a5d0a5
willybrandt-costa <161944331+willybrandt-costa@users.noreply.github.com>
2024-11-22 13:55:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ let pulseCount = 0;
 
 const heart = document.getElementById('heart');
 const flowers = document.getElementById('flowers');
+const message = document.getElementById('message');
 
 heart.addEventListener('animationiteration', () => {
     pulseCount++;
     if (pulseCount === 5) {
-        heart.style.display = 'none';
-        flowers.classList.remove('hidden');
+        heart.style.animation = "explode 0.5s ease-in-out forwards";
+        setTimeout(() => {
+            heart.style.display = 'none';
+            flowers.classList.remove('hidden');
+            setTimeout(() => {
+                flowers.style.display = 'none';
+                message.classList.remove('hidden');
+            }, 2000); // Tempo para exibir as flores antes do texto
+        }, 500); // Tempo para a explosão
     }
 });
